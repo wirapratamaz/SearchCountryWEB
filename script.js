38167d10084f1cce26f2cf3948d9bb0d471f8b0c
wirapratamaz <wirabagus185@gmail.com>
2022-06-24 16:02:37 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(finalURL);
+fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data[0]);
+        console.log(data[0].capital[0]);
+        console.log(data[0].flags.svg);
+        console.log(data[0].name.common);
+        console.log(data[0].continents[0]);
+    result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Ibu Kota:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Kontingen:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+      `;
+    })
+});
